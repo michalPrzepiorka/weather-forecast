a7d519f62237b2ac13f1d81a2c925ffb8e048fd0
Michał Przepiórka <michal.przepiorka0@gmail.com>
2020-03-15 23:14:16 +0100

    Add swagger 2 configuration class

---
+++ src/main/java/michal/weatherForecast/config/SpringFoxConfig.java
@@ -0,0 +1,28 @@
+package michal.weatherForecast.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * @author Y510p
+ * @project weatherForecast
+ * @date 15.03.2020
+ **/
+
+@Configuration
+@EnableSwagger2
+public class SpringFoxConfig {
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+}
