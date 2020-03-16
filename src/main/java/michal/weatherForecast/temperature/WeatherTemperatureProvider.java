5de86a3d40750c2d175543ff9d5aa1346eb27cb5
Michał Przepiórka <michal.przepiorka0@gmail.com>
2020-03-16 23:19:17 +0100

    Add new class WeatherTemperatureProvider

---
+++ src/main/java/michal/weatherForecast/temperature/WeatherTemperatureProvider.java
@@ -0,0 +1,42 @@
+package michal.weatherForecast.temperature;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * @author Y510p
+ * @project weatherForecast
+ * @date 16.03.2020
+ **/
+
+public class WeatherTemperatureProvider {
+
+    private List<WeatherTemperatureHelper> getCityTemperature() throws IOException {
+        final Document document;
+        List<WeatherTemperatureHelper> weather = new LinkedList<>();
+
+        try {
+            document = Jsoup.connect("https://pogoda.interia.pl/").get();
+            Elements element = document.getElementsByClass("weather-places-group cities-weather is-legend");
+            weather.clear();
+
+            for (Element row : element.select("ul").select("li")) {
+                Element index = row.getElementsByClass("weather-index-item").first();
+
+                String cityName = index.getElementsByClass("weather-index-item-name saveCityInCookie").first().text();
+                String temp = index.selectFirst("span").text();
+                weather.add(new WeatherTemperatureHelper(cityName, temp));
+            }
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return weather;
+    }
+}
